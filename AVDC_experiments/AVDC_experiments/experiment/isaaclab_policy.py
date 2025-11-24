Use ROS quaternions -> Robot now predicts correct grasp location

AVDC_experiments/AVDC_experiments/experiment/isaaclab_policy.py
@@ -118,7 +118,7 @@ class IsaacMyPolicyCL:
         pos = self.robot.data.body_pos_w[self.env_index, self.hand_body_id]
         if isinstance(pos, torch.Tensor):
             pos = pos.detach().cpu().numpy()
-        return np.asarray(pos, dtype=np.float32) + np.array([-0.5, 0, 0])
+        return np.asarray(pos, dtype=np.float32)
 
     def _initialize_plan(self):
         grasp, transforms = self.calculate_next_plan()
@@ -152,8 +152,8 @@ class IsaacMyPolicyCL:
             subgoals.append(next_subgoal)
         print("subgoals")
         print(subgoals)
-        # return subgoals
-        return [np.array([0, 0, 1])]
+        return subgoals
+        # return [np.array([0, 0, 1])]
 
     def _resize_rgb(self, image: np.ndarray) -> np.ndarray:
         width, height = self.resolution
@@ -276,6 +276,7 @@ class IsaacMyPolicyCL:
 
         start = time.time()
         _, _, flow_images, flow, _ = pred_flow_frame(self.flow_model, images, device="cuda:0")
+
         self._output_flow_image(flow_images)
         time_flow = time.time() - start
 
@@ -343,11 +344,11 @@ class IsaacMyPolicyCL:
             print("placing above object")
             return self.grasp + np.array([0.0, 0.0, 0.2])
         # drop end effector down on top of object
-        if not self.grasped and abs(pos_curr[2] - self.grasp[2]) > 0.04:
+        if not self.grasped and abs(pos_curr[2] - self.grasp[2]) > 0.06:
             print("dropping down on top of object")
             return self.grasp
         # grab object (if in grasp mode)
-        if not self.grasped and abs(pos_curr[2] - self.grasp[2]) <= 0.04:
+        if not self.grasped and abs(pos_curr[2] - self.grasp[2]) <= 0.06:
             print("grabbing object")
             self.grasped = True
             return self.grasp

AVDC_experiments/AVDC_experiments/experiment/myutils.py
@@ -343,8 +343,6 @@ def get_transforms(seg, depth, cmat, flows=[], ransac_tries=100, ransac_threshol
     center_2ds = []
     sampless = []
     samples_2d = sample_from_mask(seg, 500)
-    print("samples_2d")
-    print(samples_2d)
     sampless.append(samples_2d)
     samples_3d = to_3d(samples_2d, depth, cmat)
     grasp = get_grasp(samples_2d, depth, cmat)
@@ -352,13 +350,22 @@ def get_transforms(seg, depth, cmat, flows=[], ransac_tries=100, ransac_threshol
     # print(samples_3d.shape)
     
     points1_uv = samples_2d
+    print("points1_uv")
+    print(points1_uv.mean(axis=0))
     points1 = samples_3d
     center = grasp
     for i in range(len(flows)):
         flow = flows[i]
+        print("flow shape")
+        print(flow.shape)
+        print("flow")
+        print(np.percentile(np.linalg.norm(flow, axis=2), [50, 95, 99]))
+        print(flow[144, 148])
         center_uv = to_2d(center, cmat)[0]
         center_2ds.append(center_uv)
         points2_uv = warp_points(flow, points1_uv)
+        print("point2_uv")
+        print(points2_uv.mean(axis=0))
         t0 = time.time()
         _, inliners = ransac(points1_uv, center_uv, points2_uv, ransac_tries, ransac_threshold)
         t1 = time.time()

scripts/rsl_rl/play_avdc.py
@@ -196,7 +196,7 @@ def main():
         video_dir = None
         start_time = time.time()
         for step in range(1500):
-            repeat = 10
+            repeat = 20
             action = policy.get_action(obs)
             env_action = torch.as_tensor(action[None, :], device=env.unwrapped.device)
             for sub in range(repeat):